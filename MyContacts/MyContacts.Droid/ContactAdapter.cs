60080f8a603c4fdb2871e4c33a38d7b2cbf84ea8
Jon Cortez <jcortez@gmail.com>
2015-03-24 23:57:08 -0700

    Removed unused using statements.

--- MyContacts/MyContacts.Droid/ContactAdapter.cs
+++ MyContacts/MyContacts.Droid/ContactAdapter.cs
@@ -1,19 +1,10 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
-using Android.Content.Res;
-using Android.OS;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using Java.Net;
 using MyContacts.Core.Models;
-using Uri = Android.Net.Uri;
 
 namespace MyContacts.Droid
 {
